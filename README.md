eceee201ab58502fcea027e718d13a8161d14d61
Alex <thelanzolini@gmail.com>
2017-05-27 18:09:41 -0400

    added render methods and default image/sound

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Extra Life Alert
 
 super simple extra life alerts. scrapes the JSON from the extra life site based on participant/team ID in the URL.
+
+
+# Turorial
+
+1. Signed up with [Extra Life](https://www.extra-life.org)
+2. Navigate to your profile page
+3. Copy your participantID out of the URL
+4. Go to http://lanzo.space/extralifealert?participantID=[YOUR participantID HERE]
--- app.js
+++ app.js
@@ -1,3 +1,108 @@
-document.addEventListener('DOMContentLoaded', function(){
-  console.log('dom ready');
-});
+function openAlertWindow() {
+  window.open(location.href, location.href, 'menubar=0,resizable=1,width=1000,height=600,titlebar=no');
+}
+
+function init() {
+  var APP = document.getElementById('app');
+  var participantID = window.location.search.replace('?', '').split('=')[1];
+
+  function renderTutorial(){
+    console.log('rendering the tutorial');
+    var title = document.createElement('h1');
+    title.innerHTML = 'Extra Life Alerts';
+    APP.appendChild(title);
+
+    var notParticipantSteps = [
+      'Sign up with extra life <a target="_blank" href="http://extra-life.org">Extra Life</a>',
+      'Navigate to your profile page',
+      'Copy your participantID from the URL',
+      'Go to http://lanzo.space/extralifealert?participantID=[YOUT participantID HERE]'
+    ]
+    var steps = [
+      'Set up window capture on this page (If using chrome and OBS, you might have to disable hardware acceleration due to black screen -> Settings -> Scroll Down -> Show Advanced Settings -> System -> Uncheck "Use Hardware acceleration when available")',
+      'Start Streaming! Your Donations will appear in this window when you get them!'
+    ]
+    var ol = document.createElement('ol');
+    if(!participantID) {
+      notParticipantSteps.forEach(function(step){
+        var li = document.createElement('li');
+        li.innerHTML = step;
+        ol.appendChild(li);
+      });
+    }
+    steps.forEach(function(step){
+      var li = document.createElement('li');
+      li.innerHTML = step;
+      ol.appendChild(li);
+    });
+    APP.appendChild(ol);
+    var settingsButton = document.createElement('button');
+    settingsButton.innerHTML = 'All Done? Configure Notification >';
+    settingsButton.addEventListener('click', function(){
+      STATE.changeState('SETTINGS');
+    });
+
+    APP.appendChild(settingsButton);
+  }
+  function renderSettings(){
+    console.log('rendering the settings');
+    var title = document.createElement('h1');
+    title.innerHTML = 'Settings';
+    APP.appendChild(title);
+
+    var customImageWrapper = document.createElement('div');
+    var customImageLabel = document.createElement('label');
+    customImageLabel.innerHTML = 'Custom Image URL';
+    var customImageInput = document.createElement('input');
+    customImageWrapper.appendChild(customImageLabel);
+    customImageWrapper.appendChild(customImageInput);
+
+    var customAudioWrapper = document.createElement('div');
+    var customAudioLabel = document.createElement('label');
+    customAudioLabel.innerHTML = 'Custom Audio URL';
+    var customAudioInput = document.createElement('input');
+    customAudioWrapper.appendChild(customAudioLabel);
+    customAudioWrapper.appendChild(customAudioInput);
+
+    APP.appendChild(customImageWrapper);
+    APP.appendChild(customAudioWrapper);
+
+    var feedButton = document.createElement('button');
+    feedButton.innerHTML = 'Save and go to Feed';
+    feedButton.addEventListener('click', function(){
+      STATE.changeState('FEED');
+    });
+
+    APP.appendChild(feedButton);
+
+  }
+
+  function renderFeed(){
+    console.log('rendering the feed');
+  }
+
+  var config = {
+    image: 'http://lanzo.space/extralifealert/fbLogo.jpg',
+    audio: 'http://lanzo.space/extralifealert/thanks.ogg'
+  }
+
+  var STATE = {
+    TUTORIAL: { renderMethod: renderTutorial },
+    SETTINGS: { renderMethod: renderSettings },
+    FEED: { renderMethod: renderFeed },
+    currentState: 'TURORIAL',
+    changeState: function(state){
+      APP.classList.remove(STATE.currentState.toLowerCase());
+      STATE.currentState = state;
+      APP.classList.add(STATE.currentState.toLowerCase());
+      while (APP.hasChildNodes()) {
+        APP.removeChild(APP.lastChild);
+      }
+      STATE[state].renderMethod();
+    }
+  }
+  STATE.changeState('TUTORIAL');
+  window.STATE = STATE;
+}
+
+document.addEventListener('DOMContentLoaded', init);

ad4ccc01789bc1bdd5d21ad1c7b3b2ba6f89a77f
Alex <thelanzolini@gmail.com>
2017-05-27 20:38:40 -0400

    ALL OF THE FUNCITONALITY

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
+var test = false;
+var donationsQueue = [];
+var test2 = false;
+var CHECK_INTERVAL = 5000;
+var donationsInterval;
+var fetchInterval;
+
 function openAlertWindow() {
   window.open(location.href, location.href, 'menubar=0,resizable=1,width=1000,height=600,titlebar=no');
+}
+
+function processDonations(donations) {
+  var promise = new Promise(function(resolve, reject){
+    var sliced = donations.slice(0, 10);
+    var currentTime = new Date().getTime();
+    if(test) {
+      var q = new Date().getTime() - 5;
+      sliced.push({
+        message: "Hello World",
+        createdOn: q,
+        donorName: 'thelanzolini',
+        avatarImageURL: "//assets.donordrive.com/clients/extralife/img/avatar-constituent-default.gif",
+        donationAmount: 5
+      });
+      test2 = true;
+    }
+
+    test = true;
+
+    sliced.forEach(function(donation, index){
+      var donationTime = new Date(donation.createdOn).getTime();
+      if(currentTime - donationTime < CHECK_INTERVAL){
+        donationsQueue.push(donation);
+      }
+    });
+
+    donationsCache = sliced;
+    resolve(sliced);
+  });
+  return promise;
+}
+
+function fetchRecentDonations(participantID) {
+  if(test2) return;
+  return window.fetch('https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participantDonations&participantID='+ participantID +'&format=json')
+    .then(function(response){
+      return response.json();
+    })
+    .then(processDonations)
+  ;
 }
 
 function init() {
@@ -54,6 +102,7 @@ function init() {
     var customImageLabel = document.createElement('label');
     customImageLabel.innerHTML = 'Custom Image URL';
     var customImageInput = document.createElement('input');
+    customImageInput.setAttribute('type', 'text');
     customImageWrapper.appendChild(customImageLabel);
     customImageWrapper.appendChild(customImageInput);
 
@@ -61,15 +110,26 @@ function init() {
     var customAudioLabel = document.createElement('label');
     customAudioLabel.innerHTML = 'Custom Audio URL';
     var customAudioInput = document.createElement('input');
+    customAudioInput.setAttribute('type', 'text');
     customAudioWrapper.appendChild(customAudioLabel);
     customAudioWrapper.appendChild(customAudioInput);
 
+    var tooltip = document.createElement('div');
+    tooltip.innerHTML = 'Keep fields blank to default image/audio';
+
+    APP.appendChild(tooltip);
     APP.appendChild(customImageWrapper);
     APP.appendChild(customAudioWrapper);
 
     var feedButton = document.createElement('button');
     feedButton.innerHTML = 'Save and go to Feed';
     feedButton.addEventListener('click', function(){
+      if(!!customImageInput.value){
+        config.image = customImageInput.value;
+      }
+      if(!!customAudioInput.value){
+        config.audio = customImageInput.value;
+      }
       STATE.changeState('FEED');
     });
 
@@ -79,10 +139,43 @@ function init() {
 
   function renderFeed(){
     console.log('rendering the feed');
+    console.log(config);
+    var donationAlert = document.createElement('div');
+    donationAlert.classList.add('hidden', 'donation-alert');
+    var donationText = document.createElement('div');
+
+    var audio = document.createElement('audio');
+    audio.src = config.audio;
+
+    var donationImg = document.createElement('img');
+    donationImg.src = config.image;
+
+    donationAlert.appendChild(donationImg);
+    donationAlert.appendChild(donationText);
+
+    APP.appendChild(donationAlert);
+
+    fetchInterval = setInterval(function(){
+      fetchRecentDonations(participantID).then(console.log)
+    }, CHECK_INTERVAL);
+
+    donationsInterval = setInterval(function(){
+      var donation = donationsQueue.pop();
+      if(!!donation){
+        console.log("NEW DONATION", donation);
+        audio.play();
+        donationAlert.classList.add('fade');
+        donationText.innerHTML = donation.message;
+        setTimeout(function(){
+          donationText.innerHTML = '';
+        }, 4500);
+      }
+    }, 5000);
+
   }
 
   var config = {
-    image: 'http://lanzo.space/extralifealert/fbLogo.jpg',
+    image: 'http://lanzo.space/extralifealert/controller_blue.png',
     audio: 'http://lanzo.space/extralifealert/thanks.ogg'
   }
 
